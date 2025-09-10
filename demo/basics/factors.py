f1e95f4f12988921a6de7599ff241055680d0980
srikanthpragada <srikanthpragada@gmail.com>
2025-09-10 19:56:13 +0530

    10-SEP

--- demo/basics/factors.py
+++ demo/basics/factors.py
@@ -1,8 +1,13 @@
 
 num = int(input("Enter a number :"))
 
+found = False
 for i in range(2, num // 2 + 1):
     if num % i == 0:
+        found = True
         print(i)
+
+if not found:
+    print('Sorry! No factors found!')
 
 
