c91f51fe3f3f41ad709027cf2cdad271e7790d35
srikanthpragada <srikanthpragada@gmail.com>
2025-09-12 19:55:39 +0530

    12-SEP

---
+++ demo/ds/common_chars.py
@@ -0,0 +1,8 @@
+
+s1 = "abcdef"
+s2 = "defchg"
+
+for c in s1:
+    if c in s2:
+        print(c)
+
---
+++ demo/ds/count_digits.py
@@ -0,0 +1,8 @@
+st = "Python 3.13.7"
+
+count = 0
+for c in st:
+    if c.isdigit():
+        count += 1
+
+print(count)
